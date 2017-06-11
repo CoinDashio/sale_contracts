39aae59b2ec50350b692ae6ec303d1d52566eb85
alon muroch <alon@coindash.io>
2017-06-11 19:56:42 +0300

    Deployment changes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
+var coindash_company_account = '0x003BF8c0F98543a36D5f39Fd3EFE9bA2d4D294ec';
+
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations, {from: coindash_company_account});
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,23 @@
 var Contribution = artifacts.require("./Contribution.sol")
 var CDTMultiSigWallet = artifacts.require("./CDTMultiSigWallet.sol")
-var coindash_company_account = web3.eth.accounts[0];
+
 var privstarttime = web3.eth.getBlock('latest').timestamp;
 var pubstartTime = privstarttime + 3600;
 
-var signers = ['0x7ecf34ed29ede66ecc1068b398102aa57ccbd317', '0xead02d42efe27ff850e242892577336de3d20cf9', '0xcfd980c4115825d6270d840cab900e15a65e3436'];
+var coindash_company_account = '0x003BF8c0F98543a36D5f39Fd3EFE9bA2d4D294ec';
+var signers = [coindash_company_account, '0x006D7bB57f664BaCD12736672FB05FD5AB4677Ae', '0x001Babe793665324BB66d6651f7abAb41C7e685A'];
 
 module.exports = function(deployer) {
 
   console.log("\nDeployment:\nPrivate Start Time: " + privstarttime + "\nPublic Start Time " + pubstartTime);
 
-  deployer.deploy(CDTMultiSigWallet, signers, 2)
+  deployer.deploy(CDTMultiSigWallet, signers, 2, {from: coindash_company_account})
 	.then(function() {
 	  return deployer.deploy(Contribution,
 	  	CDTMultiSigWallet.address,
 	  	coindash_company_account,
 	  	pubstartTime,
-	  	privstarttime);
+	  	privstarttime, {from: coindash_company_account});
 	})  
 };
 
